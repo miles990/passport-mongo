707f25b8b8f3402c19eb45c4a91e5be9528103dc
AlexLee <alexlee7171@gmail.com>
2015-02-07 03:07:46 +0800

    set db reference to mongolabs

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//'url' : 'mongodb://admin:password@ds039411.mongolab.com:39411/shopping-cart-db'//無法登入時可能port被擋
-	'url' : 'mongodb://localhost/test'
+	'url' : 'mongodb://admin:password@ds039411.mongolab.com:39411/shopping-cart-db'//無法登入時可能port被擋
+	//'url' : 'mongodb://localhost/test'
 }
